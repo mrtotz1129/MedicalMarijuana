86955e546c46f39fe449ab31838451222a44079b
kenlayug <ken_layug@yahoo.com>
2016-03-29 06:45:22 +0800

    requirement crud -- ok

---
+++ app/RequirementModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RequirementModel extends Model
+{
+	protected $table		=	'tblRequirement';
+	protected $primaryKey	=	'intRequirementId';
+}
--- resources/views/maintenance-requirements.blade.php
+++ resources/views/maintenance-requirements.blade.php
@@ -7,7 +7,7 @@
 				</div>
 				<div class="col s6 right">
 					<a class="right waves-effect waves-light modal-trigger btn-floating btn-large red darken-2 left white-text tooltipped" 
-					href="#create" style="position: relative; top: 40px; right: 1%;" 
+					href="#createModal" style="position: relative; top: 40px; right: 1%;" 
 					data-tooltip="Create"><i class="material-icons">add</i></a>
 				</div>
 			</div>	
@@ -23,15 +23,13 @@
 				        </thead>
 				        	
 				        <tbody>
-				        	@foreach($fees as $fee)
+				        	@foreach($requirementList as $requirement)
 				        	<tr>
-				        		<td>{!! $fee->strFeeTypeName !!}</td>
-				        		<td>{!! $fee->strFeeName !!}</td>
-				        		<td>{!! $fee->txtFeeDesc !!}</td>
-				        		<td>{!! number_format($fee->dblPrice, 2) !!}</td>
+				        		<td>{!! $requirement->strRequirementName !!}</td>
+				        		<td>{!! $requirement->txtRequirementDesc !!}</td>
 				        		<td>
-				        			<a href="javascript:updateId({!! $fee->intFeeId !!})" class="tooltipped" data-tooltip="Update Fee Details"><i class="material-icons">mode_edit</i></a>
-				        			<a href="javascript:deactivateId({!! $fee->intFeeId !!})" class="tooltipped" data-tooltip="Deactivate Fee Details"><i class="material-icons">delete</i></a>
+				        			<a href="javascript:updateId({!! $requirement->intRequirementId !!})" class="tooltipped" data-tooltip="Update Requirement Details"><i class="material-icons">mode_edit</i></a>
+				        			<a href="javascript:deactivateId({!! $requirement->intRequirementId !!})" class="tooltipped" data-tooltip="Deactivate Requirement Details"><i class="material-icons">delete</i></a>
 				        		</td>
 				        	</tr>
 				        	@endforeach
@@ -54,9 +52,9 @@
 				</script>
 
 
-				<!-- Create Fee Modal -->
+				<!-- Create Requirement Modal -->
 				   <div id="createModal" class="modal modal-fixed-footer">
-				    <form class="col s12 form" method="post" id="createEmpForm" action="{!! url('fee') !!}" enctype="multipart/form-data">
+				    <form class="col s12 form" method="post" id="createEmpForm" action="{!! url('requirement') !!}" enctype="multipart/form-data">
 				      <div class="modal-content" style="padding-bottom: 0px !important;">
 				        <!-- <div class="container"> -->
 				      <div class="wrapper">
@@ -71,13 +69,13 @@
 				                         <label class="red-text left">(*) Indicates required field</label>
 				                    </div>
 				                   <div class="input-field col s12">
-				                        <input name="" placeholder="Ex: Benigno" id="feeID" type="text" class="validate tooltipped specialname" required data-position="bottom" data-delay="30" data-tooltip="Ex: Benigno( At least 2 or more characters )" pattern="^[a-zA-Z\-'`\s]{2,}$" maxlength="15" minlength="2">
+				                        <input name="strRequirementName" placeholder="Ex: Benigno" id="feeID" type="text" class="validate tooltipped specialname" required data-position="bottom" data-delay="30" data-tooltip="Ex: Benigno( At least 2 or more characters )" pattern="^[a-zA-Z\-'`\s]{2,}$" maxlength="15" minlength="2">
 				                        <label for="feeID" class="active">Requirement Name<span class="red-text"><b>*</b></span></label>
 				                    </div>
                      		      	 <div class="input-field col s6">
                            	           <i class="material-icons prefix">mode_edit</i>
-                           	           <textarea id="remarks" class="materialize-textarea" name="txtFeeDesc"></textarea>
-                           	           <label for="remarks">Description</label>
+                           	           <textarea id="remarks" class="materialize-textarea" name="txtRequirementDesc"></textarea>
+                           	           <label for="remarks" id="lblDesc">Description</label>
                            	         </div>
 				                </div>
 				              </div>
@@ -91,15 +89,15 @@
 				      </form>
 				</div>
 
-				<!-- Update Fee Modal -->
+				<!-- Update Requirement Modal -->
 				   <div id="updateModal" class="modal modal-fixed-footer">
-				    <form class="col s12 form" method="post" id="updateFeeForm" action="createEmployee" enctype="multipart/form-data">
-				      <input type="hidden" id="updateFeeFormToken" value="{!! csrf_token() !!}" />
+				    <form class="col s12 form" method="post" id="updateRequirementForm" action="createEmployee" enctype="multipart/form-data">
+				      <input type="hidden" id="updateRequirementFormToken" value="{!! csrf_token() !!}" />
 				      <div class="modal-content" style="padding-bottom: 0px !important;">
 				        <!-- <div class="container"> -->
 				      <div class="wrapper">
 				        <div class="input-field col s12">
-				              <h4 class="grey-text text-darken-1 center	">Update Fee</h4>
+				              <h4 class="grey-text text-darken-1 center	">Update Requirement</h4>
 				        </div>
 				                <!-- second -->
 				                  <div class="row">
@@ -107,13 +105,13 @@
 				                         <label class="red-text left">(*) Indicates required field</label>
 				                    </div>
 				                   <div class="input-field col s12">
-				                        <input name="" placeholder="Ex: Benigno" id="feeID" type="text" class="validate tooltipped specialname" required data-position="bottom" data-delay="30" data-tooltip="Ex: Benigno( At least 2 or more characters )" pattern="^[a-zA-Z\-'`\s]{2,}$" maxlength="15" minlength="2">
-				                        <label for="feeID" class="active">Requirement Name<span class="red-text"><b>*</b></span></label>
+				                        <input name="" placeholder="Ex: Benigno" id="requirementName_update" type="text" class="validate tooltipped specialname" required data-position="bottom" data-delay="30" data-tooltip="Ex: Benigno( At least 2 or more characters )" pattern="^[a-zA-Z\-'`\s]{2,}$" maxlength="15" minlength="2">
+				                        <label for="requirementName_update" class="active">Requirement Name<span class="red-text"><b>*</b></span></label>
 				                    </div>
                      		      	 <div class="input-field col s6">
                            	           <i class="material-icons prefix">mode_edit</i>
-                           	           <textarea id="remarks" class="materialize-textarea" name="txtFeeDesc"></textarea>
-                           	           <label for="remarks">Description</label>
+                           	           <textarea id="requirementDesc_update" class="materialize-textarea" name="txtFeeDesc"></textarea>
+                           	           <label for="requirementDesc_update">Description</label>
                            	         </div>
 				                </div>
 				              </div>
@@ -123,7 +121,7 @@
 				       
 				        <div class="modal-footer">
 				          <button type="reset" value="Reset" class=" modal-action modal-close waves-effect waves-purple transparent btn-flat">CANCEL</button>
-				          <button class="waves-effect waves-light indigo darken-3 white-text btn-flat" type="submit" value="Submit">CREATE</button>
+				          <button class="waves-effect waves-light indigo darken-3 white-text btn-flat" type="submit" value="Submit">UPDATE</button>
 				      </div>
 				      </form>
 				    </div>
@@ -131,44 +129,15 @@
 		</div>
 	</article>
 
-<!-- add option -->
-   <div id="feeTypeCreateModal" class="modal" style="margin-top: 30px;">
-     <form id="fee_create_form">
-     	<input type="hidden" id="fee_create_token" value="{!! csrf_token() !!}" />
-       <div class="modal-content">
-         <h4>Add Another Fee Type</h4>
-         <div class="row">
-           <div class="col s12">
-             <div class="input-field col s8 offset-s2">
-               <select id="feeTypeCreateSelect" class="browser-default" size="10">
-				@foreach($feeTypes as $feeType)
-				<option value="{!! $feeType->intFeeTypeId !!}">{!! $feeType->strFeeTypeName !!}</option>
-                @endforeach
-               </select>
-             </div>
-             <div class="input-field col s8 offset-s2" style="margin-top: 20px;">
-               <input type="text" class="validate tooltipped specialoption" placeholder="Ex: Cashier" id="fee_type_create" name="addOptionName" data-position="bottom" data-delay="30" data-tooltip="Ex: Cashier<br/>( At least 5 or more characters )" pattern="^[A-Za-z-\s]{5,}$">
-               <label for="addOptionName" class="active">New Fee</label>
-             </div>
-             <div class="input-field col s8 offset-s2 center">
-               <button type="submit" value="Submit" id="createAddPosition" class="waves-effect waves-light purple darken-3 btn-flat white-text">SAVE</button>
-             
-             </div>
-           </div>
-         </div>
-       </div>
-     </form>
-   </div>
-
 {{-- Modal Deactivate START --}}
-<div id="deactivate_fee_modal" class="modal">
-	<input type="hidden" id="deactivate_employee_token" value="{!! csrf_token() !!}" />
+<div id="deactivate_requirement_modal" class="modal">
+	<input type="hidden" id="deactivate_requirement_token" value="{!! csrf_token() !!}" />
     <div class="modal-content">
       <h4>Deactivate Fee Details</h4>
       <p>Are you sure?</p>
     </div>
     <div class="modal-footer">
-      <a class="modal-action waves-effect waves-green btn-flat" id="deactivate_fee_btn">Yes</a>
+      <a class="modal-action waves-effect waves-green btn-flat" id="deactivate_requirement_btn">Yes</a>
       <a class="modal-action modal-close waves-effect waves-green btn-flat">No</a>
     </div>
 </div>
@@ -177,14 +146,12 @@
 <script type="text/javascript">
 	function updateId(id) {
 		$.ajax({
-			url: 'fee/' + id,
+			url: 'requirement/' + id,
 			type: 'GET',
 			success: function(data) {
-				document.getElementById('slct1').value = data.intFeeTypeIdFK;
-				document.getElementById('feeName').value = data.strFeeName;
-				document.getElementById('feePrice').value = data.dblPrice;
-				document.getElementById('remarksEdit').value = data.txtFeeDesc;
-				document.getElementById('remarksEditLabel').setAttribute('class', 'active');
+				document.getElementById('requirementName_update').value = data.strRequirementName;
+				document.getElementById('requirementDesc_update').value = data.txtRequirementDesc;
+				document.getElementById('lblDesc').setAttribute('class', 'active');
 
 				$('#updateModal').openModal();
 			},
@@ -193,22 +160,20 @@
 			}
 		});
 
-		$('#updateFeeForm').on('submit', function(event) {
+		$('#updateRequirementForm').on('submit', function(event) {
 			event.preventDefault();
 
 			$.ajax({
-				url: "fee/" + id,
+				url: "requirement/" + id,
 				type: "POST",
 				data: {
 					_method: "PUT",
-					_token: document.getElementById('updateFeeFormToken').value,
-					feeType: document.getElementById('slct1').value,
-					feeName: document.getElementById('feeName').value,
-					feePrice: document.getElementById('feePrice').value,
-					feeDesc: document.getElementById('remarksEdit').value
+					_token: document.getElementById('updateRequirementFormToken').value,
+					requirementName: document.getElementById('requirementName_update').value,
+					requirementDesc: document.getElementById('requirementDesc_update').value
 				},
 				success: function(data) {
-					window.location.href = "{!! url('fee') !!}";
+					window.location.href = "{!! url('requirement') !!}";
 				},
 				error: function(xhr) {
 					console.log(xhr);
@@ -218,54 +183,27 @@
 	}
 
 	function deactivateId(id) {
-		$('#deactivate_fee_modal').openModal();
+		$('#deactivate_requirement_modal').openModal();
 
-		$('#deactivate_fee_btn').on('click', function() {
+		$('#deactivate_requirement_btn').on('click', function() {
 			$.ajax({
-				url: "fee/" + id,
+				url: "requirement/" + id,
 				type: "POST",
 				data: {
 					_method: "DELETE",
-					feeId: id
+					requirementId: id
 				},
 				success: function(data) {
-					window.location.href = "{!! url('fee') !!}";
+					window.location.href = "{!! url('requirement') !!}";
 				},
 				error: function(xhr) {
+					alert('error');
 					console.log(xhr);
 				}
 			});
 		});
 	}
 
-	$('#fee_create_form').on('submit', function(event) {
-		event.preventDefault();
-
-		$.ajax({
-			url: "{!! url('fee-type/create') !!}",
-			type: "POST",
-			data: {
-				_token: document.getElementById('fee_create_token').value,
-				feeTypeName: document.getElementById('fee_type_create').value
-			},
-			success: function(data) {
-				var option = document.createElement('option');
-				option.value = data.intFeeTypeId;
-				option.text = data.strFeeTypeName;
-				document.getElementById('feeTypeSelect').appendChild(option);
-
-				option = document.createElement('option');
-				option.value = data.intFeeTypeId;
-				option.text = data.strFeeTypeName;
-				document.getElementById('feeTypeCreateSelect').appendChild(option);
-
-				$('#feeTypeCreateModal').closeModal();
-			},
-			error: function(xhr) {
-				console.log(xhr);
-			}
-		});
-	});	
 </script>
  
 @endsection

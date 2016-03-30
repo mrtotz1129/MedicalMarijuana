668e68f51a8f24aea16944a90c77d8559c0fa678
kenlayug <ken_layug@yahoo.com>
2016-03-30 22:35:56 +0800

    pos ongoing

---
+++ app/InventoryModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class InventoryModel extends Model
+{
+    protected $table		=	'tblInventory';
+    protected $primaryKey	=	'intInventoryId';
+}

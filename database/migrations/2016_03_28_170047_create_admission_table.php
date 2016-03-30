32e8a8b4cdbce13037a5a1230394496b59e1d01d
whizdummy <jeraldjohnpormon@yahoo.com>
2016-03-30 12:39:55 +0800

    Modified admission migration

--- database/migrations/2016_03_28_170047_create_admission_table.php
+++ database/migrations/2016_03_28_170047_create_admission_table.php
@@ -24,6 +24,7 @@ class CreateAdmissionTable extends Migration
             $table->string('strAdmissionRemarks');
             $table->integer('intDoctorIdFK')
                 ->unsigned();
+            $table->timestamps();
 
             $table->foreign('intPatientIdFK')
                 ->references('intPatientId')

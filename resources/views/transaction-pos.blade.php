0acba76c10df0a59dfa57a091bd97f95f708500b
killandibuz <killandibuz@gmail.com>
2016-03-30 21:15:20 -0700

    POS Updates

--- resources/views/transaction-pos.blade.php
+++ resources/views/transaction-pos.blade.php
@@ -1,31 +1,50 @@
 @extends('maintenance')
 @section('article')
 <article class="white main z-depth-1">
-<br>
-	<div class="row">
-		<div class="col s6">
-			<h3 class="thin indigo-text text-darken-2 col left">Cashier</h3>
-			<a href="#billOut" class="modal-trigger">BILL OUT</a>
-		</div>
-	</div>
-    <br>
+	<div class="row indigo darken-2" style="margin-left: -30px; border-top-right-radius: 10px;">
+			<div class="col s6">
+				<h4 class="thin white-text">Out Patient Pharmacy</h4>
+			</div>
+			<div class="col s6 right">
+				<a class="right waves-effect waves-light modal-trigger btn-floating btn-large green darken-2 left white-text tooltipped" 
+				href="#itemDetails" style="position: relative; top: 40px; right: 1%;" 
+				data-tooltip="Create"><i class="material-icons">shopping_cart</i></a>
+			</div>
+	</div>	
+	<br>
     <div class="container">
-    	<table id="billTbl" class="display" cellspacing="0" width="100%">
-            <thead>
-                <tr>
-                    <th>Patient Number</th>
-                    <th>Name</th>
-                    <th>Address</th>
-                    <th>Contact NO.</th>
-                    <th>Room No.</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>  	
-    </table>
+    	<div class="row">
+    		<div class="col s6">
+    			<h4 class="thin">Available Items</h4>
+				<table id="itemTable" class="display" cellspacing="0" width="100%">
+			        <thead>
+			            <tr>
+			                <th>Item Name</th>
+			                <th>Price</th>
+			                <th style="width: 20px;">Action</th>
+			            </tr>
+			        </thead>  	
+				</table>
+    		</div>
+
+    		<div class="col s6">
+    			<h4 class="thin">My Cart</h4>
+				<table id="itemTable" class="display" cellspacing="0" width="100%">
+			        <thead>
+			            <tr>
+			                <th>Item Name</th>
+			                <th>Price</th>
+			                <th style="width: 20px;">Action</th>
+			            </tr>
+			        </thead>  	
+				</table>
+    		</div>
+    	</div>
+    	
     </div>
-    <script type="text/javascript">
+    <script type="text/javascript">  
     	$(document).ready(function() {
-    	    $('#billTbl').DataTable( {
+    	    $('#itemTable').DataTable( {
     	        dom: 'Bfrtip',
     	        buttons: [
     	            'copyHtml5',
@@ -118,5 +137,33 @@
 	      </div>
 	      </form>
 	</div>
+	
+	<!-- add option -->
+   <div id="itemDetails" class="modal" style="margin-top: 30px;">
+     <form id="createEquipmentTypeForm">
+     	<input type="hidden" id="createEquipmentTypeFormToken" value="{!! csrf_token() !!}" />
+       <div class="modal-content">
+         <h4>Details</h4>
+         <div class="row">
+           <div class="col s12">
+             <div class="input-field col s8 offset-s2">
+               <select id="unitOfMeasure" class="browser-default" size="10">
+                 @foreach($equipmentTypes as $equipmentType)
+                 <option value="">Measure 1</option>
+                 @endforeach
+               </select>
+             </div>
+             <div class="input-field col s8 offset-s2" style="margin-top: 20px;">
+               <input type="number" class="validate tooltipped specialoption" placeholder="Ex: 100" id="itemQuantity" name="createEquipmentType" data-position="bottom" data-delay="30" data-tooltip="Ex: Cashier<br/>( At least 5 or more characters )" pattern="^[A-Za-z-\s]{5,}$">
+               <label for="itemQuantity" class="active"> Quantity</label>
+             </div>
+             <div class="input-field col s8 offset-s2 center">
+               <button type="submit" value="Submit" id="createAddPosition" class="waves-effect waves-light purple darken-3 btn-flat white-text">SAVE</button>
+	             </div>
+	           </div>
+	         </div>
+	       </div>
+	     </form>
+	   </div>
 </article>
 @endsection
